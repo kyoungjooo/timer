d8569e7f0ab49872b3b6f0eadf6f731141d3c57b
leekjoo <leekjoo1008@gmail.com>
2024-12-03 13:37:01 +0900

    chore: setting folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { css } from "@emotion/css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 타이머 만들기
+      00:00:00
+      23:59:59
+      start, record, stop, pause
+
+      start를 누르면 초가 1초씩 증가한다.
+      record 를 누르면 아래 현지 HH:MM:SS 기록, 누적
+      stop을 누르면 0초로 돌아가면서 stop
+      pause를 누르면 일시정지 다시 start 누르면 재실행
+
+      setInterval, clearInterval, useRef, props 사용
+      */}
     </div>
   );
 }
